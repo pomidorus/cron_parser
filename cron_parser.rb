72f282b2c2cc5fdac3f4c724b8b42b0710821c20
Andrii Seleznov <andreii.seleznov@gmail.com>
2018-08-04 15:03:45 +0100

    spec & logic CronParser for simple case

--- cron_parser.rb
+++ cron_parser.rb
@@ -1,11 +1,68 @@
 class CronParser
-  attr_reader :string, :minute
+  attr_reader :string, :minute, :hour, :day_of_month, :month, :day_of_week, :command, :error
 
   def initialize(string)
     @string = string
+    @error = false
   end
 
   def parse
+    begin
+      @minute = parsed_data[:minute]
+      @hour = parsed_data[:hour]
+      @day_of_month = parsed_data[:day_of_month]
+      @month = parsed_data[:month]
+      @day_of_week = parsed_data[:day_of_week]
+      @command = parsed_data[:command]
+    rescue ArgumentError
+      @error = true
+      @error_message = 'Input string is invalid'
+    end
+  end
 
+  private
+
+  def parsed_data
+    @data ||= {
+      minute: parse_minute(splitted_string[0]),
+      hour: parse_hour(splitted_string[1]),
+      day_of_month: parse_day_of_month(splitted_string[2]),
+      month: parse_month(splitted_string[3]),
+      day_of_week: parse_day_of_week(splitted_string[4]),
+      command: parse_command(splitted_string[5])
+    }
+  end
+
+  def splitted_string
+    @splitted ||= string.split(' ', 6)
+  end
+
+  def parse_minute(string)
+    raise ArgumentError if !(string =~ /[a-zA-Z]/).nil?
+    [string.to_i]
+  end
+
+  def parse_hour(string)
+    raise ArgumentError if !(string =~ /[a-zA-Z]/).nil?
+    [string.to_i]
+  end
+
+  def parse_day_of_month(string)
+    raise ArgumentError if !(string =~ /[a-zA-Z]/).nil?
+    [string.to_i]
+  end
+
+  def parse_month(string)
+    raise ArgumentError if !(string =~ /[a-zA-Z]/).nil?
+    [string.to_i]
+  end
+
+  def parse_day_of_week(string)
+    raise ArgumentError if !(string =~ /[a-zA-Z]/).nil?
+    [string.to_i]
+  end
+
+  def parse_command(string)
+    string
   end
 end
--- spec/cron_parser_spec.rb
+++ spec/cron_parser_spec.rb
@@ -3,36 +3,48 @@ require_relative '../cron_parser'
 describe CronParser do
   describe '#parse' do
     let(:parser) { CronParser.new(string) }
-    let(:string) { '1 2 3 4 5 /usr/bin/find' }
 
-    it 'parse the correct minute' do
-      parser.parse
-      expect(parser.minute).to eq(1)
+    context 'when string is invalid' do
+      let(:string) { 'a 2 3 4 5 /usr/bin/find' }
+
+      it 'returns error' do
+        parser.parse
+        expect(parser.error).to eq(true)
+      end
     end
 
-    it 'parse the correct hour' do
-      parser.parse
-      expect(parser.hour).to eq(2)
-    end
+    context 'when string is valid' do
+      let(:string) { '1 2 3 4 5 /usr/bin/find' }
 
-    it 'parse the correct day of month' do
-      parser.parse
-      expect(parser.day_of_month).to eq(3)
-    end
+      it 'parse the correct minute' do
+        parser.parse
+        expect(parser.minute).to eq([1])
+      end
 
-    it 'parse the correct month' do
-      parser.parse
-      expect(parser.month).to eq(4)
-    end
+      it 'parse the correct hour' do
+        parser.parse
+        expect(parser.hour).to eq([2])
+      end
 
-    it 'parse the correct day of week' do
-      parser.parse
-      expect(parser.day_of_week).to eq(5)
-    end
+      it 'parse the correct day of month' do
+        parser.parse
+        expect(parser.day_of_month).to eq([3])
+      end
 
-    it 'parse the correct command' do
-      parser.parse
-      expect(parser.command).to eq(command)
+      it 'parse the correct month' do
+        parser.parse
+        expect(parser.month).to eq([4])
+      end
+
+      it 'parse the correct day of week' do
+        parser.parse
+        expect(parser.day_of_week).to eq([5])
+      end
+
+      it 'parse the correct command' do
+        parser.parse
+        expect(parser.command).to eq('/usr/bin/find')
+      end
     end
   end
 end
